133ce28f6c2244be5be7ee9f85d9c16686ed9eae
randiprince <u1431008@umail.utah.edu>
2022-09-27 21:17:13 -0600

    day 2 lab hello world in java

---
+++ Day2/HelloJava/src/Array.java
@@ -0,0 +1,11 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int intArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int element: intArray) {
+            System.out.println(element);
+        }
+    }
+
+}
