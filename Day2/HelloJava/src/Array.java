7ea263d91dd643f2a7ad0d4f903b015e30cddcee
randiprince <u1431008@umail.utah.edu>
2022-09-27 21:19:26 -0600

    day 2 lab hello world added printed out sum

--- Day2/HelloJava/src/Array.java
+++ Day2/HelloJava/src/Array.java
@@ -1,11 +1,15 @@
 public class Array {
 
     public static void main(String[] args) {
+        int sum = 0;
         int intArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         for (int element: intArray) {
             System.out.println(element);
+            sum += element;
         }
+
+        System.out.println(sum);
     }
 
 }
