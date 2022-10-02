dcb4ccf25c3844cb1f3167bb0cb4b423eeb7e81b
randiprince <u1431008@umail.utah.edu>
2022-10-02 17:52:58 -0600

    fractions

--- Day2/HelloJava/src/Array.java
+++ Day2/HelloJava/src/Array.java
@@ -2,7 +2,7 @@ public class Array {
 
     public static void main(String[] args) {
         int sum = 0;
-        int intArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         for (int element: intArray) {
             System.out.println(element);
@@ -10,6 +10,7 @@ public class Array {
         }
 
         System.out.println(sum);
+        System.out.println("Hello world!");
     }
 
 }
